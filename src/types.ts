ad0e72c0da33e53b8e5dcbd827bbb4bfa139c0d8
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-21 17:16:08 +0200

    Added Patient type

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,12 @@ export interface Diagnose {
     name: string,
     latin?: string;
 };
+
+export interface Patient {
+    id: number,
+    name: string,
+    dateOfBirth: string,
+    ssn: number,
+    gender: string,
+    occupation: string
+};
