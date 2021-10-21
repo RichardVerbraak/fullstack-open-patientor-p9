30c7a6b81daf0dfd5736b6d673167a44d52d9a37
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-21 17:28:18 +0200

    Diagnose route now working, was coding in the wrong file (patients instead of diagnose file)

--- src/routes/patientRoutes.ts
+++ src/routes/patientRoutes.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import { getDiagnoses } from '../services/diagnoseService';
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
-    const diagnoses = getDiagnoses()
-    res.send(diagnoses);
+   res.send('Fetching patients');
 });
 
 
