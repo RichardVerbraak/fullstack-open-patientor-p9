914db7421a1eae10025551217f2719bd27fd479b
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-29 17:04:51 +0200

    Date parse function added

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,11 @@
 import { newPatient } from "./types"
 
-const parseNewPatient = (patientData: unknown) : newPatient => {
+const parseNewPatient = (patientData: any) : newPatient => {
+    const {name, dateOfBirth, ssn, gender, occupation} = patientData
+
     const parsedPatient = {
         name: parseString(name),
-        dateOfBirth,
+        dateOfBirth: parseDate(dateOfBirth),
         ssn,
         gender,
         occupation
@@ -12,7 +14,7 @@ const parseNewPatient = (patientData: unknown) : newPatient => {
     return parsedPatient;
 }
 
-const isString = ((name: any) : name is string => {
+const isString = ((name: unknown) : name is string => {
     return typeof name === 'string' || name instanceof String 
 })
 
@@ -23,5 +25,18 @@ const parseString = ((name: unknown) : string => {
 
     return name
 })
+
+const isDate = (date: string) : boolean => {
+    return Boolean(Date.parse(date))
+}
+
+// isString operation runs first so that's why isDate can take the date in as string since it comes AFTER parsing for a string
+const parseDate = (date: unknown) : string => {
+    if(!date || !isString(date) || !isDate(date)) {
+        throw new Error(`Missing date or invalid data`)
+    }
+
+    return date
+}
 
 export { parseNewPatient }
