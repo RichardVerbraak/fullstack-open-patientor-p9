f9f1782f37f07641163685928c199b9558c11121
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-10-29 16:54:46 +0200

    Parse string function added

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,27 @@
-const addNewPatient = () => {
+import { newPatient } from "./types"
 
+const parseNewPatient = (patientData: unknown) : newPatient => {
+    const parsedPatient = {
+        name: parseString(name),
+        dateOfBirth,
+        ssn,
+        gender,
+        occupation
+    }
+
+    return parsedPatient;
 }
 
-export { addNewPatient }
+const isString = ((name: any) : name is string => {
+    return typeof name === 'string' || name instanceof String 
+})
+
+const parseString = ((name: unknown) : string => {
+    if(!name || !isString(name)) {
+        throw new Error(`Missing name or invalid data`)
+    }
+
+    return name
+})
+
+export { parseNewPatient }
