83785d4b5feab22169c7dc8f057868cdbc6df77b
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-11-01 16:23:17 +0100

    Added parseOccupation

--- src/utils.ts
+++ src/utils.ts
@@ -9,7 +9,7 @@ const parseNewPatient = (patientData: any) : newPatient => {
         dateOfBirth: parseDate(dateOfBirth),
         ssn: parseSSN(ssn),
         gender: parseGender(gender),
-        occupation
+        occupation: parseOccupation(occupation)
     }
 
     return parsedPatient;
@@ -19,6 +19,14 @@ const isString = ((name: unknown) : name is string => {
     return typeof name === 'string' || name instanceof String 
 })
 
+const isDate = (date: string) : boolean => {
+    return Boolean(Date.parse(date))
+}
+
+const isGender = (gender: any) : gender is Gender => {
+    return Object.values(Gender).includes(gender)
+}
+
 const parseString = ((name: unknown) : string => {
     if(!name || !isString(name)) {
         throw new Error(`Missing name or invalid data -- ${name}`)
@@ -26,10 +34,6 @@ const parseString = ((name: unknown) : string => {
 
     return name
 })
-
-const isDate = (date: string) : boolean => {
-    return Boolean(Date.parse(date))
-}
 
 // isString operation runs first so that's why isDate can take the date in as string since it comes AFTER parsing for a string
 const parseDate = (date: unknown) : string => {
@@ -48,16 +52,20 @@ const parseSSN = (ssn: unknown) : string => {
     return ssn
 }
 
-const isGender = (gender: any) : gender is Gender => {
-    return Object.values(Gender).includes(gender)
-}
-
-const parseGender = (gender: unknown) : Gender => {
+const parseGender = (gender: any) : Gender => {
     if(!gender || !isGender) {
         throw new Error(`Missing gender or invalid data -- ${gender}`)
     }
 
     return gender
+}
+
+const parseOccupation = (occupation: unknown) : string => {
+    if(!occupation || !isString(occupation)) {
+        throw new Error(`Missing occupation or invalid data -- ${occupation}`)
+    }
+
+    return occupation
 }
 
 export { parseNewPatient }
