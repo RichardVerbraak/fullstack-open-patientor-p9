600d9457f0a2ea0af685e69e5bf6a209aa3876c8
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-11-10 17:15:51 +0100

    Comments on type parameters not being the same as validating data

--- patientor/src/App.tsx
+++ patientor/src/App.tsx
@@ -1,50 +1,57 @@
-import React from "react";
-import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
-import { Button, Divider, Header, Container } from "semantic-ui-react";
+import React from 'react'
+import axios from 'axios'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { Button, Divider, Header, Container } from 'semantic-ui-react'
 
-import { apiBaseUrl } from "./constants";
-import { useStateValue } from "./state";
-import { Patient } from "./types";
+import { apiBaseUrl } from './constants'
+import { useStateValue } from './state'
+import { Patient } from './types'
 
-import PatientListPage from "./PatientListPage";
+import PatientListPage from './PatientListPage'
 
 const App = () => {
-  const [, dispatch] = useStateValue();
-  React.useEffect(() => {
-    void axios.get<void>(`${apiBaseUrl}/ping`);
+	// Only using dispatch here
+	const [, dispatch] = useStateValue()
 
-    const fetchPatientList = async () => {
-      try {
-        const { data: patientListFromApi } = await axios.get<Patient[]>(
-          `${apiBaseUrl}/patients`
-        );
-        dispatch({ type: "SET_PATIENT_LIST", payload: patientListFromApi });
-      } catch (e) {
-        console.error(e);
-      }
-    };
-    void fetchPatientList();
-  }, [dispatch]);
+	React.useEffect(() => {
+		void axios.get<void>(`${apiBaseUrl}/ping`)
 
-  return (
-    <div className="App">
-      <Router>
-        <Container>
-          <Header as="h1">Patientor</Header>
-          <Button as={Link} to="/" primary>
-            Home
-          </Button>
-          <Divider hidden />
-          <Switch>
-            <Route path="/">
-              <PatientListPage />
-            </Route>
-          </Switch>
-        </Container>
-      </Router>
-    </div>
-  );
-};
+		const fetchPatientList = async () => {
+			try {
+				// Type Parameter for the axios.get function which tells us we expect to receive an Array of Patient objects
+				// !!! This is not the same as validating data, this merely says this is what the data is supposed to look like
+				// but this does not mean this is validated
+				// To validate you could either take they entire payload and then return the correct types or use a type guard or use a library like io-ts
+				const { data: patientListFromApi } = await axios.get<Patient[]>(
+					`${apiBaseUrl}/patients`
+				)
 
-export default App;
+				dispatch({ type: 'SET_PATIENT_LIST', payload: patientListFromApi })
+			} catch (e) {
+				console.error(e)
+			}
+		}
+		void fetchPatientList()
+	}, [dispatch])
+
+	return (
+		<div className='App'>
+			<Router>
+				<Container>
+					<Header as='h1'>Patientor</Header>
+					<Button as={Link} to='/' primary>
+						Home
+					</Button>
+					<Divider hidden />
+					<Switch>
+						<Route path='/'>
+							<PatientListPage />
+						</Route>
+					</Switch>
+				</Container>
+			</Router>
+		</div>
+	)
+}
+
+export default App
