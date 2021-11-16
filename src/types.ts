6a364f7850e239626b96cc257f6c5cc70c67ee6d
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-11-16 16:00:52 +0100

    Changed the hardcoded data to another sample and added a base type for Entry

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,34 @@
 export enum Gender {
-    Male = 'male',
-    Female = 'female',
-    Other = 'other'
+	Male = 'male',
+	Female = 'female',
+	Other = 'other',
 }
 
-export interface Diagnose {
-    code: string,
-    name: string,
-    latin?: string;
+export interface Diagnosis {
+	code: string
+	name: string
+	latin?: string
+}
+
+export interface BaseEntry {
+	id: string
+	description: string
+	date: string
+	specialist: string
+	diagnosisCodes?: Array<Diagnosis['code']>
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {    
-}
+export interface Entry {}
 
 export interface Patient {
-    id: string,
-    name: string,
-    dateOfBirth: string,
-    ssn: string,
-    gender: Gender,
-    occupation: string
-    entries: Entry[]
+	id: string
+	name: string
+	dateOfBirth: string
+	ssn: string
+	gender: Gender
+	occupation: string
+	entries: Entry[]
 }
 
 // Could also be used when parsing a new patient in the parsePatient utils function
@@ -29,4 +36,4 @@ export type newPatient = Omit<Patient, 'id'>
 
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>
 
-export type nonSensitivePatient = Omit<Patient, 'ssn'>;
+export type nonSensitivePatient = Omit<Patient, 'ssn'>
