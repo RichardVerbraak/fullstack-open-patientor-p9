b4928110356d200465456c65e2c1d0c856b0574c
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-11-19 17:01:31 +0100

    Description added to the /api/diagnoses endpoint

--- src/routes/diagnoseRoutes.ts
+++ src/routes/diagnoseRoutes.ts
@@ -1,12 +1,13 @@
-import express from 'express';
-import { getDiagnoses } from '../services/diagnoseService';
+import express from 'express'
+import { getDiagnoses } from '../services/diagnoseService'
 
-const router = express.Router();
+const router = express.Router()
 
+//  @desc    Get all of the diagnoses data
+//  @route   GET /api/diagnoses
 router.get('/', (_req, res) => {
-    const diagnoses = getDiagnoses();
-    res.send(diagnoses);
-});
+	const diagnoses = getDiagnoses()
+	res.send(diagnoses)
+})
 
-
-export default router;
+export default router
