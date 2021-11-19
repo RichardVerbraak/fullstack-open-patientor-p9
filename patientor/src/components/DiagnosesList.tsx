7189d9ac086c7b41c9398d1ac1db6b851ea9b045
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-11-19 15:46:07 +0100

    DiagnosesList component added to render the diagnoses if the patient has any in their entry

--- patientor/src/components/DiagnosesList.tsx
+++ patientor/src/components/DiagnosesList.tsx
@@ -1,27 +1,46 @@
 import React from 'react'
+import { useStateValue } from '../state'
 import { Diagnosis } from '../types'
 
-const DiagnosesList = (diagnoses: Diagnosis[]) => {
+// Correct way of handling props in React
+// Source: https://stackoverflow.com/questions/47805206/reactjs-typescript-cannot-pass-array-of-items-as-property-to-child
+const DiagnosesList: React.FC<{ diagnoseCodes: Array<Diagnosis['code']> }> = ({
+	diagnoseCodes,
+}: {
+	diagnoseCodes: Array<Diagnosis['code']>
+}) => {
+	const [{ diagnoses }] = useStateValue()
+
+	console.log(diagnoseCodes)
+
 	// Create an array of diagnose codes by using .find in diagnoses to find the same code and return the found diagnose object
 	// Find always returns Type | undefined and because of such the key of the list item will be bugged
 	// No clue how to fix
-	// const diagnosisCodes = diagnoses?.map((code): Diagnosis | undefined => {
-	// 	const sameCodeAsPatient = diagnoses.find((diagnose) => {
-	// 		return diagnose.code === code
-	// 	})
+	const sameCodesAsPatient = diagnoseCodes?.map(
+		(code): Diagnosis | undefined => {
+			return diagnoses.find((diagnose) => {
+				return diagnose.code === code
+			})
+		}
+	)
 
-	// 	return sameCodeAsPatient
-	// })
-
-	return <div></div>
+	return (
+		<div>
+			{
+				<ul>
+					{sameCodesAsPatient.map(
+						(diagnoseObj): React.ReactElement<Diagnosis> => {
+							return (
+								<li key={diagnoseObj?.code}>
+									{diagnoseObj?.code} <span>{diagnoseObj?.name}</span>
+								</li>
+							)
+						}
+					)}
+				</ul>
+			}
+		</div>
+	)
 }
-
-// {diagnosisCodes?.map((diagnoseObj): React.ReactElement<Diagnosis> => {
-// 	return (
-// 		<li key={diagnoseObj?.code}>
-// 			{diagnoseObj?.code} <span>{diagnoseObj?.name}</span>
-// 		</li>
-// 	)
-// })}
 
 export default DiagnosesList
--- patientor/src/components/OccupationalEntry.tsx
+++ patientor/src/components/OccupationalEntry.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { Diagnosis, OccupationalHealthCareEntry } from '../types'
+import { OccupationalHealthCareEntry } from '../types'
 import { Card, Icon } from 'semantic-ui-react'
-import { useStateValue } from '../state'
+import DiagnosesList from './DiagnosesList'
 
 const OccupationalEntry: React.FC<{ entry: OccupationalHealthCareEntry }> = ({
 	entry,
 }) => {
-	const [{ diagnoses }] = useStateValue()
-
 	return (
 		<Card>
 			<Card.Content>
@@ -16,23 +14,7 @@ const OccupationalEntry: React.FC<{ entry: OccupationalHealthCareEntry }> = ({
 				</Card.Header>
 				<Card.Meta>{entry.description}</Card.Meta>
 				{entry.diagnosisCodes && (
-					<ul>
-						{entry.diagnosisCodes
-							?.map((code): Diagnosis | undefined => {
-								const sameCodeAsPatient = diagnoses.find((diagnose) => {
-									return diagnose.code === code
-								})
-
-								return sameCodeAsPatient
-							})
-							.map((diagnoseObj): React.ReactElement<Diagnosis> => {
-								return (
-									<li key={diagnoseObj?.code}>
-										{diagnoseObj?.code} <span>{diagnoseObj?.name}</span>
-									</li>
-								)
-							})}
-					</ul>
+					<DiagnosesList diagnoseCodes={entry.diagnosisCodes} />
 				)}
 			</Card.Content>
 		</Card>
